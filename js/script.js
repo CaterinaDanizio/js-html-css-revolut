8a8f34ecd44572927bf8508090a30a9d89db398a
Caterina <danizio@outlook.it>
2020-11-05 10:04:25 +0100

    Update

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@ $( document ).ready(function() {
 var dropdownAttivo = $("li.active")
    $("li.drop-item").click(function () {
       $(this).find(".dropdown").toggleClass("active");
-    // if(!$(this).hasClass("active")){
-    //     $(".dropdown").removeClass("active");
-    //     }
-    //     else {
-    //       $(this).addClass("active");
-    //     }
+    if(!$(this).hasClass("active")){
+        $(".dropdown").removeClass("active");
+        $(this).find(".dropdown").addClass("active");
+        }
       });
 
  });
